afcdadbac2543294ab62f103f0b03a983ea44edc
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-01-24 23:51:16 +0100

    gave a good layout with some funcionality to VocabularyForm

--- src/components/VocabularyForm.js
+++ src/components/VocabularyForm.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function VocabularyForm() {
+
+  const [word, setWord] = useState('');
+  const [explanation, setExplanation] = useState('');
+  const [example, setExample] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('word, explanation, example', word, explanation, example)
+
+
+    setWord('');
+    setExplanation('');
+    setExample('');
+  }
   return (
-    <form>
-      <input type="text" placeholder='Das Wort' />
-      <input type="text" placeholder='Die Bedeutung' />
-      <textarea cols="45" rows="5" placeholder='Das Beispiel'></textarea>
-      <button>Speichern</button>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group mt-3">
+        <input value={word} onChange={(e)=>setWord(e.target.value)} type="text" className="form-control" placeholder="Das Wort" />
+      </div>
+      <div className="form-group mt-3">
+        <input
+        value={explanation} onChange={(e)=>setExplanation(e.target.value)}
+          type="text"
+          className="form-control"
+          placeholder="Die Bedeutung"
+        />
+      </div>
+      <div className="form-group mt-3">
+        <textarea
+        value={example} onChange={(e)=>setExample(e.target.value)}
+          className="form-control"
+          cols="45"
+          rows="5"
+          placeholder="Das Beispiel"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary mt-3">Speichern</button>
     </form>
-  )
+  );
 }
