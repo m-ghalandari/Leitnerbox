28b12763c4086954e2b5429ba0068bf4164443aa
M.ghalandari <mohammadghalandari1998@gmail.com>
2023-03-03 19:46:02 +0100

    deleted all unusful comments

--- src/App.js
+++ src/App.js
@@ -125,66 +125,6 @@ function App() {
     }
   };
 
-  //hier wird alle Flashcard ihre level um 1 erhöht
- /* const changeFlashcardsLevels = async (cards) => {
-
-    let box1 = false;
-    let box2 = false;
-    let box3 = false;
-    let box4 = false;
-
-    cards.forEach(card => {
-      if (card.box === 1 && card.level >= 2) {
-        box1 = true;
-      }
-      if (card.box === 2 && card.level >= 4) {
-        box2 = true;
-      }
-      if (card.box === 3 && card.level >= 8) {
-        box3 = true;
-      }
-      if (card.box === 4 && card.level >= 16) {
-        box4 = true;
-      }
-    });
-    if (box1) {
-      alert("Warnung: Mindestens eine Karte aus Box 1 hat das maximale Level erreicht. Es muss zuerst letzte Level frei machen.");
-      return;
-    }
-    if (box2) {
-      alert("Warnung: Mindestens eine Karte aus Box 2 hat das maximale Level erreicht. Es muss zuerst letzte Level frei machen.");
-      return;
-    }
-    if (box3) {
-      alert("Warnung: Mindestens eine Karte aus Box 3 hat das maximale Level erreicht. Es muss zuerst letzte Level frei machen.");
-      return;
-    }
-    if (box4) {
-      alert("Warnung: Mindestens eine Karte aus Box 4 hat das maximale Level erreicht. Es muss zuerst letzte Level frei machen.");
-      return;
-    }
-
-    cards.map((card) => {
-      setFlashcards((prevFlashcards) =>
-        prevFlashcards.map((flashcard) => {
-          if (flashcard.id === card.id) {
-            return { ...flashcard, level: card.level + 1 };
-          }
-          return flashcard;
-        })
-      );
-    });
-
-    axios.put('http://84.150.35.142:3001/api/increaseLevels', {
-      cards
-    }).then(() => { })
-      .catch(err => {
-        alert(err);
-      })
-
-  };
-  */
-
     //hier wird alle Flashcard ihre level um 1 erhöht
     const changeFlashcardsLevels = async (cards) => {
 
