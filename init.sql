fc36c6d16b918171d5b577716d8dcb2a47a0652a
Mamali99 <mohammadghalandari1998@gmail.com>
2025-08-27 13:57:18 +0200

    added a init.sql file into root project

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS cards;
+USE cards;
+
+CREATE TABLE IF NOT EXISTS flashcards (
+  id VARCHAR(255) PRIMARY KEY,
+  box INT NOT NULL,
+  level INT NOT NULL,
+  front TEXT,
+  back TEXT,
+  example TEXT
+);
